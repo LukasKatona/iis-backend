bb019fc62f694dcb1ca07541c6d4b247ba60aa8c
L-Sinister <klarkakejdova@gmail.com>
2024-11-01 10:56:19 +0100

    feat: NewCategoryRequest(get, post, patch)

--- api/NewCategoryRequestApi.py
+++ api/NewCategoryRequestApi.py
@@ -1,42 +1,46 @@
 from fastapi import APIRouter, HTTPException
+from typing import List, Optional
 from sqlmodel import Session, create_engine, select
 
-
-from entities.ProductCategory import ProductCategory
 from entities.NewCategoryRequest import NewCategoryRequest
-from enums.CategoryRequestState import Category
-
+from enums.CategoryRequestState import CategoryRequestState
 from constants.databaseURL import DATABASE_URL
 
 router = APIRouter()
-
 db = create_engine(DATABASE_URL)
 
-@router.get("/product-categories")
-def get_product_categories() -> list[ProductCategory]:
+@router.get("/category-requests", response_model=List[NewCategoryRequest], tags=["Category Requests"])
+def get_category_requests() -> List[NewCategoryRequest]:
     with Session(db) as session:
-        categories = session.exec(select(ProductCategory)).all()
-        return categories
+        category_requests = session.exec(select(NewCategoryRequest)).all()
+        return category_requests
 
-@router.post("/category-request", response_model=NewCategoryRequest)
-def create_category_request(new_request: NewCategoryRequest) -> NewCategoryRequest:
+@router.post("/category-request", response_model=NewCategoryRequest, tags=["Category Requests"])
+def create_category_request(newCategoryName: str, createdById: int, parentCategoryId: Optional[int] = None, state: CategoryRequestState = CategoryRequestState.PENDING ) -> NewCategoryRequest:
     with Session(db) as session:
-        existing_category = session.exec(select(NewCategoryRequest).where(NewCategoryRequest.name == new_request.name)).first()
-        if existing_category:
-            raise HTTPException(status_code=400, detail="Category with this name already exists.")
+        # existing_request = session.exec(
+        #     select(NewCategoryRequest).where(NewCategoryRequest.newCategoryName == newCategoryName)
+        # ).first()
+        
+        # if existing_request:
+        #     raise HTTPException(status_code=400, detail="Category request with this name already exists.")
+        
+        new_request = NewCategoryRequest(
+            newCategoryName=newCategoryName,
+            parentCategoryId=parentCategoryId,
+            createdById=createdById,
+            state=state
+        )
         
         session.add(new_request)
         session.commit()
         session.refresh(new_request)
         return new_request
 
-@router.patch("/category-request/{category_id}/status", response_model=NewCategoryRequest)
-def update_category_request_status(category_id: int, new_state: Category) -> NewCategoryRequest:
+@router.patch("/category-request/{category_id}/status", response_model=NewCategoryRequest, tags=["Category Requests"])
+def update_category_request_status(category_id: int, new_state: CategoryRequestState) -> NewCategoryRequest:
     with Session(db) as session:
         category_request = session.get(NewCategoryRequest, category_id)
-        if not category_request:
-            raise HTTPException(status_code=404, detail="Category request not found.")
-        
         category_request.state = new_state
         session.commit()
         session.refresh(category_request)
